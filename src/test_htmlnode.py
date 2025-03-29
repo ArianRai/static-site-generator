5c850074617bdd780fc0395624f2d54a91d3db18
ArianRai <arianraiesteban_20@hotmail.com>
2025-03-29 22:17:21 +0100

    markdown to html

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -114,7 +114,7 @@ class TestHTMLNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "Just a text value")
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -29,17 +29,16 @@ class TextNode:
 
 
 def text_node_to_html_node(text_node):
-    if text_node.text_type not in TextType:
-        raise Exception("Text type not valid")
     if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, "Just a text value")
+        return LeafNode(None, text_node.text)
     if text_node.text_type == TextType.BOLD:
-        return LeafNode("b", "Just a text value")
+        return LeafNode("b", text_node.text)
     if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", "Just a text value")
+        return LeafNode("i", text_node.text)
     if text_node.text_type == TextType.CODE:
-        return LeafNode("code", "Just a text value")
+        return LeafNode("code", text_node.text)
     if text_node.text_type == TextType.LINK:
-        return LeafNode("a", "Just a text value", {"href": "https://boot.dev"})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", "", {"src": "https://boot.dev", "alt": "image"})
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")

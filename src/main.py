b5362dda8a894565efbb4cc745dde82d16b4effd
ArianRai <arianraiesteban_20@hotmail.com>
2025-04-05 01:18:14 +0200

    basepath added, docs file, build.sh

--- src/main.py
+++ src/main.py
@@ -1,25 +1,25 @@
 import os
 import shutil
+import sys
 
 from textnode import TextNode, TextType
 from copy_files import copy_files
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 static_path = "./static"
-public_path = "./public"
+docs_path = "./docs"
 content_path = "./content"
 template_path = "./template.html"
 
 
 def main():
-    if os.path.exists(public_path):
-        shutil.rmtree(public_path)
-    copy_files(static_path, public_path)
-    generate_page(
-        os.path.join(content_path, "index.md"),
-        template_path,
-        os.path.join(public_path, "index.html"),
-    )
+    basepath = sys.argv[1]
+    if basepath == None:
+        basepath = "/"
+    if os.path.exists(docs_path):
+        shutil.rmtree(docs_path)
+    copy_files(static_path, docs_path)
+    generate_pages_recursive(content_path, template_path, docs_path, basepath)
 
 
 main()
